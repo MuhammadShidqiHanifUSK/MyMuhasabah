1d19d25e7f4a51b2bec7a53da9311a9e64fff2ce
Muhammad Shidqi Hanif <shidqi24@mhs.usk.ac.id>
2026-05-28 13:40:24 +0700

    feat: update model User dengan relasi dan fillable lengkap

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role', 'avatar', 'bio'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -28,5 +28,17 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // Relasi ke Muhasabah
+    public function muhasabahs()
+    {
+        return $this->hasMany(Muhasabah::class);
+    }
+
+    // Relasi ke Tracker
+    public function trackers()
+    {
+        return $this->hasMany(Tracker::class);
     }
 }

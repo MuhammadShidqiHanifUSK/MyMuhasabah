0244978f70af6f95052d863a235f9aef110de8c3
Muhammad Shidqi Hanif <shidqi24@mhs.usk.ac.id>
2026-05-28 13:23:58 +0700

    feat: schema migration tabel trackers

--- database/migrations/2026_05_28_061520_create_trackers_table.php
+++ database/migrations/2026_05_28_061520_create_trackers_table.php
@@ -6,20 +6,59 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('trackers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->date('tanggal');
+
+            // Sholat Wajib
+            $table->boolean('shubuh')->default(false);
+            $table->boolean('dzuhur')->default(false);
+            $table->boolean('ashar')->default(false);
+            $table->boolean('maghrib')->default(false);
+            $table->boolean('isya')->default(false);
+
+            // Sholat Sunnah
+            $table->boolean('sunnah_qabliyah_shubuh')->default(false);
+            $table->boolean('sunnah_qabliyah_dzuhur')->default(false);
+            $table->boolean('sunnah_badiyah_dzuhur')->default(false);
+            $table->boolean('sunnah_qabliyah_ashar')->default(false);
+            $table->boolean('sunnah_qabliyah_maghrib')->default(false);
+            $table->boolean('sunnah_badiyah_maghrib')->default(false);
+            $table->boolean('sunnah_qabliyah_isya')->default(false);
+            $table->boolean('sunnah_badiyah_isya')->default(false);
+            $table->boolean('tahajud')->default(false);
+            $table->boolean('dhuha')->default(false);
+            $table->boolean('witir')->default(false);
+
+            // Amalan Kebaikan Lainnya
+            $table->boolean('tilawah')->default(false);
+            $table->boolean('dzikir_pagi')->default(false);
+            $table->boolean('dzikir_petang')->default(false);
+            $table->boolean('puasa_sunnah')->default(false);
+            $table->boolean('sedekah')->default(false);
+            $table->boolean('membantu_orang')->default(false);
+            $table->boolean('silaturahmi')->default(false);
+
+            // Amal Keburukan
+            $table->boolean('berkata_kotor')->default(false);
+            $table->boolean('berbohong')->default(false);
+            $table->boolean('ghibah')->default(false);
+            $table->boolean('berkata_kasar')->default(false);
+            $table->boolean('merokok')->default(false);
+            $table->boolean('begadang_siasia')->default(false);
+            $table->boolean('scrolling_berlebihan')->default(false);
+            $table->boolean('marah_berlebihan')->default(false);
+            $table->boolean('iri_dengki')->default(false);
+            $table->boolean('sombong')->default(false);
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('trackers');

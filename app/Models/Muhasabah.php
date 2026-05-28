689b45b8e67099807ca7673518b6797fc3c02f4f
Muhammad Shidqi Hanif <shidqi24@mhs.usk.ac.id>
2026-05-28 13:34:33 +0700

    feat: isi model Muhasabah dengan fillable dan relasi

--- app/Models/Muhasabah.php
+++ app/Models/Muhasabah.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Muhasabah extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'mood',
+        'tanggal',
+    ];
+
+    protected $casts = [
+        'tanggal' => 'date',
+    ];
+
+    // Relasi ke User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
